6b37ee711e0c956b22cf920bb2826921bcd2f668
MAACarvalho <m.a.a.carvalho@outlook.com>
2018-05-14 19:12:55 +0100

    Now the execute_commands_piped is fully working and just need to figure a way to clone the information within the pipes

--- outputs.c
+++ outputs.c
@@ -69,58 +69,70 @@ void split_args (char* command, char** dest, int dest_size) {
 void execute_command (char* command) {
   char* arr_args[ARG_SIZE] = {0};
   split_args(command, arr_args, ARG_SIZE);
-  //for (int i=0; i<ARG_SIZE; i++)
-    //if (arr_args[i]) puts(arr_args[i]);
   execvp(arr_args[0], arr_args);
 }
-/*
-int guiao6ex2();
-  int fdm, fdf;
-  mkfifo("/tmp/logd", 0666);
-  if (fork() != 0) {
-    return 0;
-  }
-  fdm = open("/tmp/logmsgs", O_CREAT|O_WRONLY|O_APPEND, 0666);
-  while ((fdf = open("/tmp/logd", O_RDONLY)) != -1) {
-    int n;
-    char buffer[512];
-    n = read(fdf, buffer, sizeof(buffer));
-    write(fdm, buffer, n);
-    close(fdf);
-  }
-  return 0;
-}
-*/
-int execute_commands_piped(char* commands[], int n_comm) {
-  int fd[2];
-  pipe(fd);
 
-  for(int i=0; i<n_comm-1; i++) {
-    if (fork() == 0) {
-      dup2(fd[0],0);
-      close(fd[0]);
-      dup2(fd[1],1);
-      close(fd[1]);
-      execute_command(commands[i]);
-      _exit(-1);
-    } else {
-      close(fd[1]);
-      wait(NULL);
-      char str[BUFFER_SIZE];
-      int rd_count = read(fd[0], str, sizeof(str));
-      str[rd_count] = '\0';
-      close(fd[0]);
-      puts("------------------");
-      printf("This is what command %d put in the pipe: \n%s", i, str);
-      //write(1, str, strlen(str));
-      puts("------------------");
+void create_pipe(char* prefix, int no_pipe, char** pipe_storage) {
+  char path_to_pipe[strlen(prefix) + 11];
+  sprintf(path_to_pipe, "%s%d", prefix, no_pipe);
+  mkfifo(path_to_pipe, 0666);
+  pipe_storage[no_pipe] = malloc(sizeof(char) * strlen(path_to_pipe));
+  strcpy(pipe_storage[no_pipe], path_to_pipe);
+}
+
+int execute_commands_piped (char* commands[], int n_comm) {
+  int child_pid, i;
+  char pipe_prefix[] = "./SO_PIPE_";
+
+  char** pipes = malloc(sizeof(char) * (strlen(pipe_prefix) + 11) * n_comm);
+
+  for(i=0; i <= n_comm; i++) {
+    create_pipe(pipe_prefix, i, pipes);
+    
+    int fdw, fdr;
+    switch (child_pid = fork()) {
+      case -1:
+        perror("Couldn't create fork!");
+        _exit(-1);
+      case 0:
+        if (i!=0) {
+          while ((fdr = open(pipes[i-1], O_RDONLY, 0666)) < 0);
+          //printf("Pipe open for read: %s\n", pipes[i-1]);
+          if (i == n_comm) _exit(0);
+          dup2(fdr, 0);
+          close(fdr);
+        }
+        if (i != n_comm) {
+          fdw = open(pipes[i], O_CREAT|O_WRONLY|O_TRUNC, 0666);
+          //printf("Pipe open for write: %s\n", pipes[i]);
+          dup2(fdw, 1);
+          close(fdw);
+          execute_command(commands[i]);
+          _exit(-1);
+        }
+      default:
+        break;      
     }
   }
-  //char final[BUFFER_SIZE];
-  //if (!read(fd[0], final, sizeof(final))) return(-1);
-  //write(1, final, strlen(final)+1);
+  return(0);
+}
 
-  return 1;
+int test1 () {
+  char* str[5];
+  str[0] = "ls";
+  str[1] = "sort";
+  str[2] = "head -1";
+  str[3] = "wc -c";
+  printf("1st command: %s\n", str[0]);
+  printf("2nd command: %s\n", str[1]);
+  printf("3rd command: %s\n", str[2]);
+  printf("4th command: %s\n", str[3]);
+  execute_commands_piped(str, 1);
+  //execute_command(str[0]);
+  //execute_command(str[0]);
+  //execute_command(str[1]);
+  //execute_command(str[2]);
+  return 0;
 }
 
 int main (int argc, char* argv[]) {
@@ -131,19 +143,9 @@ int main (int argc, char* argv[]) {
     system(str);
     outputs("temp.nb");
   } else {
-    char* str[4];
-    str[0] = "ls";
-    str[1] = "sort";
-    //str[2] = "head -1";
-    printf("1st command: %s\n", str[0]);
-    printf("2nd command: %s\n", str[1]);
-    //puts("---------------------------");
-    //printf("3rd command: %s\n", str[2]);
-    execute_commands_piped(str, 2);
-//    execute_command(str[0]);
-    //execute_command(str[0]);
-    //execute_command(str[1]);
-    //execute_command(str[2]);
+    return test1();
+    //return test2();
   }
+
   return 0;
 }
